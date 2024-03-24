0c6bf407cdc34ce4e31a20c185a1ff9945a07ee1
alexquar <alexanderquarrie@gmail.com>
2024-03-24 11:33:54 -0400

    router done

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ res.sendFile('./views/index.html', {root: __dirname});
 app.get('/about', (req,res)=>{
     res.sendFile('./views/about.html', {root: __dirname});
     })
+
+app.get('/about-us', (req,res)=>{
+        res.redirect('/about');
+        })
+
+//fires on all requests but only if it makes it here (misses all get handlers)
+app.use((req,res)=>{
+res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
