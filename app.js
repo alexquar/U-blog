23799e132562fa83953b57e1cd519438785220f3
alexquar <alexanderquarrie@gmail.com>
2024-03-24 12:12:23 -0400

    flushing out site

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const express = require('express')
+const express = require('express');
 
+// express app
 const app = express();
 
-app.set('view engine', 'ejs')
+// listen for requests
+app.listen(3000);
 
-const listen = app.listen(3000);
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
-app.get('/', (req,res)=>{
-res.render('index')
-})
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
-app.get('/about', (req,res)=>{
-    res.render('about')
-    })
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
-app.get('/blogs/create', (req,res)=>{
-        res.render('create')
-        })
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-//fires on all requests but only if it makes it here (misses all get handlers)
-app.use((req,res)=>{
-res.status(404).render('404')
-})
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
