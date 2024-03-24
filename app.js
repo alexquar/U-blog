b19ac71d4b81283df2f3c52e32cf5b4feb6e79f3
alexquar <alexanderquarrie@gmail.com>
2024-03-24 11:49:30 -0400

    ejs setup

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const express = require('express')
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 const listen = app.listen(3000);
 
 app.get('/', (req,res)=>{
-res.sendFile('./views/index.html', {root: __dirname});
+res.render('index')
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about')
     })
 
-app.get('/about-us', (req,res)=>{
-        res.redirect('/about');
+app.get('/blogs/create', (req,res)=>{
+        res.render('create')
         })
 
 //fires on all requests but only if it makes it here (misses all get handlers)
 app.use((req,res)=>{
-res.status(404).sendFile('./views/404.html', {root: __dirname})
+res.status(404).render('404')
 })
