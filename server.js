662dcc14416b653556100d8d14d488c2eb90da6d
alexquar <alexanderquarrie@gmail.com>
2024-03-23 21:01:23 -0400

    req and res

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
-const http = require('http')
-const server = http.createServer( (req, res)=>{
-console.log('request')
-})
+const http = require('http');
+const fs = require('fs');
 
-server.listen(3000,'localhost',()=>{
-//listening for requests and fires a function at first
-})
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+
+
+  // routing
+  let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
+
+
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
